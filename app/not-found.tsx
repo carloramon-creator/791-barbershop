c2077fb899d9126a52d119b0ef196b8e5c5d4497
carloramon-creator <carloramon@gmail.com>
2026-01-02 15:31:02 -0300

    Fix: Simplify not-found.tsx with client component

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,8 +1,5 @@
+'use client';
+
 export default function NotFound() {
-  return (
-    <div style={{ textAlign: 'center', padding: '50px' }}>
-      <h1>404 - Página não encontrada</h1>
-      <p>Desculpe, a página que você está procurando não existe.</p>
-    </div>
-  );
+  return <h1>Not Found</h1>;
 }
