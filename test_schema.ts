8a9fedaed4be080fbd17abdd35c838bff7002677
Carlo Ramon <carloramon@gmail.com>
2026-01-11 12:15:57 -0300

    fix: remove temporary test file

--- test_schema.ts
+++
@@ -1,8 +0,0 @@
-import { supabaseAdmin } from './lib/supabase-server';
-
-async function test() {
-  const { data, error } = await supabaseAdmin.from('appointments').select('*').limit(1);
-  if (error) console.error(error);
-  else console.log(Object.keys(data[0] || {}));
-}
-test();
